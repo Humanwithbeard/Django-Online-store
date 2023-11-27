3ece470ede95886b9a3864c0be06e468278ffa31
denis ukrajev <humanwithbeard@gmail.com>
2023-11-27 19:02:08 +0200

    done

---
+++ catalog/views.py
@@ -0,0 +1,17 @@
+from django.shortcuts import render
+
+
+# Create your views here.
+def home(request):
+    return render(request, 'catalog/home.html')
+
+
+def contacts(request):
+    if request.method == 'POST':
+        # в переменной request хранится информация о методе, который отправлял пользователь
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        # а также передается информация, которую заполнил пользователь
+        print(f'Имя: {name}\nТелефон: {phone}\nСообщение: {message}')
+    return render(request, 'catalog/contacts.html')
